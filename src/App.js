d94c678da4cccd96a797bd9e0107a64406885ed2
Polyana Balotin <polybalotin@hotmail.com>
2021-09-22 16:12:42 -0300

    pagina de perfil

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Switch from 'react-bootstrap/esm/Switch';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/perfil" component={ Profile } />
     </Switch>
   );
 }
