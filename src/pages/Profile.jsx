be96d0e699f52fc986c3a97bc2c3790d9d06a5d5
Matheus <matheus.pormanhani@gmail.com>
2021-09-22 18:13:41 -0300

    Import header

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useHistory } from 'react-router';
+import Header from '../components/Header';
 
 export default function Profile() {
   const history = useHistory();
